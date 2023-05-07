c6a9da8889a73fb345d5b56ee3e998e555175cac
nurmuh221 <nurmuhammad221@gmail.com>
2023-05-07 14:42:27 +0800

    Operator Binary Aritmatika

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,19 @@
+package Operator;
+
+import java.sql.SQLOutput;
+
+public class OperatorBinary {
+
+    String nama = "Muhammad Nur";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
